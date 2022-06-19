dc42649131b26c1824c37ef0410d9212d5b4dd7f
naoya-imai <imai-naoya476@g.ecc.u-tokyo.ac.jp>
2022-06-19 15:48:45 +0900

    initial commit

---
+++ src/Register.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <>
+      <h1>新規登録</h1>
+      <form>
+        <div>
+          <label>メールアドレス</label>
+        </div>
+      </form>
+    </>
+  );
+};
